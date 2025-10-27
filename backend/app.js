b2c3ea93d0fc7bdc7636fe966279d4fb570e5213
Krish Tarani <taranikrish4@gmail.com>
2025-10-28 00:00:17 +0530

    origin change

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,13 @@ const app=express();
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173"||"https://motionoramatrix.netlify.app",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://motionoramatrix.netlify.app",
+    "https://motionoramatrix.in"
+  ],
+  credentials: true,
+}));
 
 
  //common middleware
